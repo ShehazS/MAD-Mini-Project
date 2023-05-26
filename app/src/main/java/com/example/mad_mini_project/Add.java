6b84ca5da92ad72df12c10b65ca342bbe6e679c1
SHEHAZ SUBAIR <19F18938@mec.edu.om>
2023-05-26 04:08:18 +0400

    1st

--- app/src/main/java/com/example/mad_mini_project/Add.java
+++ app/src/main/java/com/example/mad_mini_project/Add.java
@@ -6,18 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Add extends AppCompatActivity {
 
     Button add,delete,update,view,cancel;
+    EditText type,wp,rp,qty;
+    FirebaseDatabase data;
+    DatabaseReference dataref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
 
         getuserstatus();
 
@@ -26,11 +33,24 @@ public class Add extends AppCompatActivity {
         update = findViewById(R.id.Update);
         view = findViewById(R.id.View);
         cancel = findViewById(R.id.Cancel);
+        type = findViewById(R.id.type);
+        wp = findViewById(R.id.WPrice);
+        rp = findViewById(R.id.RPrice);
+        qty = findViewById(R.id.Quantity);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                data = FirebaseDatabase.getInstance();
+                dataref = data.getReference("Product");
+                String Type = String.valueOf(type.getText());
+                String WholesalePrice = String.valueOf(wp.getText());
+                String RetainPrice = String.valueOf(rp.getText());
+                String Quantity = String.valueOf(qty.getText());
+                ProductHelperClass helper = new ProductHelperClass(Type,WholesalePrice,RetainPrice,Quantity);
+
+                dataref.setValue(helper);
             }
         });
         delete.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/example/mad_mini_project/ProductHelperClass.java
@@ -0,0 +1,48 @@
+package com.example.mad_mini_project;
+
+public class ProductHelperClass {
+
+    String Type,WholesalePrice,RetainPrice,Quantity;
+
+    public ProductHelperClass() {
+    }
+
+    public ProductHelperClass(String type, String wholesalePrice, String retainPrice, String quantity) {
+        Type = type;
+        WholesalePrice = wholesalePrice;
+        RetainPrice = retainPrice;
+        Quantity = quantity;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getWholesalePrice() {
+        return WholesalePrice;
+    }
+
+    public void setWholesalePrice(String wholesalePrice) {
+        WholesalePrice = wholesalePrice;
+    }
+
+    public String getRetainPrice() {
+        return RetainPrice;
+    }
+
+    public void setRetainPrice(String retainPrice) {
+        RetainPrice = retainPrice;
+    }
+
+    public String getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        Quantity = quantity;
+    }
+}
